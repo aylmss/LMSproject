15064f9c8fd19efdd112d3d230788a2f6004df51
aylmss <altynay.yelemessova@gmail.com>
2022-06-17 19:36:50 +0600

    fixed home.html

--- myLmsApp/admin.py
+++ myLmsApp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Grade
+from .models import Grade, Chat, Module, Problem, ProblemIO, Profile
 # Register your models here.
 admin.site.register(Grade)
+admin.site.register(Chat)
+admin.site.register(Module)
+admin.site.register(Problem)
+admin.site.register(ProblemIO)
+admin.site.register(Profile)
