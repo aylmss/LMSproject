bb7924f03d5ff0137d94698e0c7a09b6d54ee4ab
aylmss <altynay.yelemessova@gmail.com>
2022-06-21 21:36:40 +0600

    added compiler not linked to db

--- myLmsApp/urls.py
+++ myLmsApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import userGradeListView, baseView, profileView, moduleListView, problemListView, problemIOView
+from .views import userGradeListView, baseView, profileView, moduleListView, problemListView
+#problemIOView
+
+from . import views
+
 urlpatterns=[  
     # path('', gradeListView.as_view(), name='home'),
     path('', baseView.as_view(), name='base'),
@@ -7,7 +11,7 @@ urlpatterns=[
     path('user_profile', profileView.as_view(), name='user_profile'),
     path('modules', moduleListView.as_view(), name='modules'),
     path('problems', problemListView.as_view(), name='problems'),
-    path('problem_io', problemIOView.as_view(), name='problem_io'),
+    path('problem_io', views.index, name='problem_io'),
+    path('runcode', views.runcode, name='runcode'),
     #path('home', mmdv.as_view(), name='home'),
-    
 ]
--- myLmsApp/views.py
+++ myLmsApp/views.py
@@ -1,3 +1,5 @@
+import sys
+
 from modulefinder import Module
 from django.shortcuts import render
 
@@ -30,10 +32,46 @@ class problemListView(ListView):
     template_name= 'problems.html'   #??? kak sdelat; nomer
     result_list=Problem.objects.all()
 
-class problemIOView(ListView):
-    model= ProblemIO
-    template_name= 'problem_io.html'   #??? kak sdelat; nomer
-    result_list=ProblemIO.objects.all()
+# class problemIOView(ListView):
+#     model= ProblemIO
+#     template_name= 'problem_io.html'   #??? kak sdelat; nomer
+#     result_list=ProblemIO.objects.all()
+
+def index(request):
+    return render(request, 'problem_io.html')
+
+def runcode(request):
+    
+    if request.method == "POST":
+        codeareadata = request.POST['codearea']
+
+        try:
+            #save original standart output reference
+
+            original_stdout = sys.stdout
+            sys.stdout = open('file.txt', 'w') #change the standard output to the file we created
+
+            #execute code
+
+            exec(codeareadata)  #example =>   print("hello world")
+
+            sys.stdout.close()
+
+            sys.stdout = original_stdout  #reset the standard output to its original value
+
+            # finally read output from file and save in output variable
+
+            output = open('file.txt', 'r').read()
+
+        except Exception as e:
+            # to return error in the code
+            sys.stdout = original_stdout
+            output = e
+
+
+    #finally return and render index page and send codedata and output to show on page
+
+    return render(request , 'problem_io.html', {"code":codeareadata , "output":output})
 
 # def mmdv(request):
 #     Gradedisplay=Grade.objects.all()
