bb30db53715ff65020989dd6f66c4d2305c0a8e7
aylmss <altynay.yelemessova@gmail.com>
2022-07-03 20:24:04 +0600

    compiler template with 2 columns

--- myLmsApp/urls.py
+++ myLmsApp/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
-from .views import  profileView, moduleListView, baseView,problemListView, problemIOView, userGradeListView
+from .views import  profileView, moduleListView, baseView,problemListView, problemIOView, userGradeListView, problemIOUpdateView
 
 from . import views
 
 urlpatterns=[  
-    # path('', gradeListView.as_view(), name='home'),
+    # path('home', gradeListView.as_view(), name='home'),
     path('', baseView.as_view(), name='base'),
     path('user_grades', userGradeListView.as_view(), name='user_grades'),
     path('user_profile', profileView.as_view(), name='user_profile'),
     path('modules', moduleListView.as_view(), name='modules'),
     path('problems', problemListView.as_view(), name='problems'),
-    path('problem_io', views.index, name='problem_io'),
     path('runcode', views.runcode, name='runcode'),
-    path('problem_io_detail/<int:pk>/', problemIOView.as_view(), name='problem_io_detail'),
+    path('problem_detail/<int:pk>/', problemIOView.as_view(), name='problem_detail'),
+    path('problem_detail/<int:pk>/update/', problemIOUpdateView.as_view(), name='problem_update'),
     path('chat', views.chat_index, name='chat'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    #path('home', mmdv.as_view(), name='home'),
+    path('base2', views.mmdv, name='base2'),
 ]
